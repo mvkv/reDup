4768306471077ae88350774523f93c8409f09e98
Francesco Maccari <f.maccari@infos.it>
2022-11-18 00:54:39 +0100

    Gets images from folders_ids list implemented downloading image given image_id, implemented getting images given list of folders,

--- backend/drive/drive_handler.py
+++ backend/drive/drive_handler.py
@@ -1,6 +1,9 @@
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+from googleapiclient.http import MediaIoBaseDownload
+
+import io
 
 
 class DriveHandler:
@@ -8,7 +11,23 @@ class DriveHandler:
         # TODO: Implement refresh token
         self.drive = build('drive', 'v3', credentials=Credentials(token))
 
-    def get_files(self, get_images=False, parent_id=None):
+    def __get_image_from_id(self, image_id: str) -> str:
+        try:
+            request = self.drive.files().get_media(fileId=image_id)
+            file = io.BytesIO()
+            downloader = MediaIoBaseDownload(file, request)
+            done = False
+            while done is False:
+                status, done = downloader.next_chunk()
+                print(F'Download {int(status.progress() * 100)}%')
+
+        except HttpError as error:
+            print(F'An error occurred: {error}')
+            file = None
+
+        return file.getvalue()
+
+    def get_folders_or_images_from_parent(self, get_images=False, parent_id="root"):
         mimeType = "application/vnd.google-apps.folder"
         if get_images:
             mimeType = "image/jpeg"
@@ -20,9 +39,9 @@ class DriveHandler:
             while not request_done:
                 print("Getting files...")
                 res = self.drive.files().list(
-                    q=f"'{'root' if not parent_id else parent_id}' in parents and mimeType = '{mimeType}' and trashed = false",
+                    q=f"'{parent_id}' in parents and mimeType = '{mimeType}' and trashed = false",
                     fields="nextPageToken, files(id, name, parents)",
-                    pageToken=page_token
+                    pageToken=page_token,
                 ).execute()
 
                 if "nextPageToken" in res:
@@ -37,3 +56,12 @@ class DriveHandler:
             files = None
 
         return files
+
+    def get_images_from_folders_ids(self, folders_ids):
+        images_ids = []
+        images = []
+        for folder_id in folders_ids:
+            images_ids += [images.append(self.__get_image_from_id(image["id"])) for image in self.get_folders_or_images_from_parent(
+                get_images=True, parent_id=folder_id)]
+
+        return images
